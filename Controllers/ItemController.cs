b950a6480e6c69ab9c331edd0d9d714c957b3761
jill <jill@epicodus.com>
2015-12-30 18:35:56 -0800

    Add CRUD for tasks

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using System.Linq;
+using ToDoRelationships.Models;
+
+namespace ToDoRelationships.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ToDoDbContext db = new ToDoDbContext();
+        public IActionResult Index()
+        {
+            return View(db.Items.Include(x => x.Category).ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            Item item = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(item);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            Item item = db.Items.FirstOrDefault(d => d.ItemId == id);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult Edit(Item item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            Item item = db.Items.FirstOrDefault(d => d.ItemId == id);
+            return View(item);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Item item = db.Items.FirstOrDefault(d => d.ItemId == id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -3,9 +3,6 @@
 ###Description
 This app uses a pseudo code-first approach with Entity Framework and allows the user to create a to do list with categories. This is done with foreign key relationships between tables in the database.
 
-##Technologies used
-ASP.NET 5, MVC 6, Entity Framework 6
-
 ###Use
 This app connects to (localdb)\mssqllocaldb server to a database with the name ToDoRelationships. 
 
@@ -23,3 +20,6 @@ The Category table will have two columns:
 - Name: data type nvarchar(50), deselect _Allow Nulls_
 
 Create a one-to-many relationship between Item and Category.
+
+####Technologies used
+ASP.NET 5, MVC 6, Entity Framework 6
